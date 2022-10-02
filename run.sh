0e530e3f3734713f4fad916ef183a3641ca2c6d6
cubicroot <github@cubicroot.xyz>
2022-10-02 17:56:18 +0200

    improve mobile css

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 sed -i "s/FULLNAME/$FULLNAME/" /usr/local/apache2/htdocs/impressum.html
 sed -i "s/STREET/$STREET/" /usr/local/apache2/htdocs/impressum.html
 sed -i "s/CITY/$CITY/" /usr/local/apache2/htdocs/impressum.html
+sed -i "s/PHONE_NUM/$PHONE_NUM/" /usr/local/apache2/htdocs/impressum.html
 
 httpd-foreground
